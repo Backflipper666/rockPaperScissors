6efd03b478ff2a456c4e8a36b21cd678bf46c375
Backflipper666 <elderly619@gmail.com>
2022-02-16 09:58:37 +0600

    Computer returns a string

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,2 +1,34 @@
-console.log("Hello world!");
 //simple game
+let choices = ["Rock", "Paper", "Scissors"];
+function computerPlay() {
+    let computerChoice = Math.floor(Math.random() * choices.length);
+    let computerChoiceString = (choices[computerChoice])[0];
+    return computerChoiceString;
+}
+
+
+
+function userPlay() {
+    let userInput = prompt("(R) for Rock, (P) for paper and (S) for scissors" ).toUpperCase();
+    return userInput;
+    
+}
+
+
+
+function gameLogic() {
+    //computer wins
+    if (computerPlay() == "R" && userPlay() == "S") {
+        console.log("Computer won");
+    } 
+    else if (computerPlay() == "P" && userPlay() == "R") {
+        console.log("Computer won");
+    }
+    else if (computerPlay() == "S" && userPlay() == "P") {
+        console.log("Computer won");
+    }
+}
+console.log(computerPlay());
+/* gameLogic();
+console.log(computerPlay());
+console.log(userPlay()); */
