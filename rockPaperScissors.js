0fca2834da9490015df02d1ca988c4900b01d80e
Backflipper666 <elderly619@gmail.com>
2022-02-19 23:03:50 +0600

    runs it but prompts user twice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,26 +9,46 @@ function computerPlay() {
 
 
 function userPlay() {
-    let userInput = prompt("(R) for Rock, (P) for paper and (S) for scissors" ).toUpperCase();
-    return userInput;
+    let userInput = prompt("(R) for Rock, (P) for paper and (S) for scissors" );
+    return userInput.toUpperCase();
     
 }
 
-
+let userTurn = userPlay();
+let computerTurn = computerPlay();
 
 function gameLogic() {
     //computer wins
     if (computerPlay() == "R" && userPlay() == "S") {
-        console.log("Computer won");
+        return console.log("Computer won, rock beats scissors");
     } 
     else if (computerPlay() == "P" && userPlay() == "R") {
-        console.log("Computer won");
+        return console.log("Computer won, paper beats rock");
     }
     else if (computerPlay() == "S" && userPlay() == "P") {
-        console.log("Computer won");
+        return console.log("Computer won, scissors beat paper");
+    }
+
+    //player wins
+    else if (userTurn == "R" && computerTurn == "S"){
+        return console.log("User won! Rock beats scissors");
+    }
+    else if (userTurn == "P" && computerTurn == "R"){
+        return console.log("User won, paper beats rock");
+    }
+    else if (userTurn == "S" && computerTurn == "P"){
+        return console.log("User won, scissors beat paper");
+    }
+
+    //draw
+    else if (userTurn == "R" && computerTurn == "R"){
+        return console.log("It's a draw, both chose Rock");
+    }
+    else if (userTurn == "P" && computerTurn == "P"){
+        return console.log("It's a draw, both chose Paper");
+    }
+    else if (userTurn == "S" && computerTurn == "S"){
+        return console.log("It's a draw, both chose Scissors");
     }
 }
-console.log(computerPlay());
-/* gameLogic();
-console.log(computerPlay());
-console.log(userPlay()); */
+gameLogic();
